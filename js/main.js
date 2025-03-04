e74841a71b4007351127a085654b159176bbe7c3
brachiel22200769 <l22200769@pachuca.tecnm.mx>
2025-03-03 20:35:25 -0600

    Actualización de main.js a nueva versión

--- js/main.js
+++ js/main.js
@@ -14,10 +14,13 @@ class Circle {
     this.posY = y;
     this.radius = radius;
     this.color = color;
-    this.text = text;
+    this.textBase = text;
+    this.textCount = 1;
     this.speed = speed;
-    this.dx = (Math.random() > 0.5 ? 1 : -1) * 10 * this.speed;
-    this.dy = (Math.random() > 0.5 ? 1 : -1) * 10 * this.speed;
+    this.dx = (Math.random() > 0.5 ? 1 : -1) * 1 * this.speed;
+    this.dy = (Math.random() > 0.5 ? 1 : -1) * 1 * this.speed;
+    this.bounceCount = 0;
+    this.maxBounces = 10;
   }
 
   draw(context) {
@@ -26,7 +29,7 @@ class Circle {
     context.textAlign = "center";
     context.textBaseline = "middle";
     context.font = "20px Arial";
-    context.fillText(this.text, this.posX, this.posY);
+    context.fillText(`${this.textBase}${this.textCount}`, this.posX, this.posY);
     context.lineWidth = 2;
     context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2, false);
     context.stroke();
@@ -34,16 +37,20 @@ class Circle {
   }
 
   update(context) {
+    if (this.bounceCount >= this.maxBounces) return; // Detener el movimiento
+
     this.draw(context);
 
     if (this.posX + this.radius >= window_width || this.posX - this.radius <= 0) {
       this.dx = -this.dx;
-      this.posX = Math.max(this.radius, Math.min(this.posX, window_width - this.radius));
+      this.bounceCount++;
+      this.textCount++;
     }
 
     if (this.posY + this.radius >= window_height || this.posY - this.radius <= 0) {
       this.dy = -this.dy;
-      this.posY = Math.max(this.radius, Math.min(this.posY, window_height - this.radius));
+      this.bounceCount++;
+      this.textCount++;
     }
 
     this.posX += this.dx;
@@ -55,14 +62,14 @@ let randomX = Math.random() * (window_width - 60) + 30;
 let randomY = Math.random() * (window_height - 60) + 30;
 let randomRadius = Math.floor(Math.random() * 50 + 20);
 
-let miCirculo = new Circle(randomX, randomY, randomRadius, "black", "Tec1", 1);
-let miCirculo2 = new Circle(randomX, randomY, randomRadius, "white", "Tec2", 1);
+let miCirculo = new Circle(randomX, randomY, randomRadius, "black", "Tec", 1);
+let miCirculo2 = new Circle(randomX, randomY, randomRadius, "white", "Tec", 1);
 
 function updateCircle() {
   requestAnimationFrame(updateCircle);
   ctx.clearRect(0, 0, window_width, window_height);
   miCirculo.update(ctx);
   miCirculo2.update(ctx);
-};
+}
 
 updateCircle();
